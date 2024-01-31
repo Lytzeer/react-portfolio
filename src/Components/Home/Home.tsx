d2b01f05422393956bcc20f370ebdad052a6019c
Lytzeer <lukasportier@gmail.com>
2024-01-31 21:49:09 +0100

    first screen

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -31,7 +31,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="bg-cblack min-h-screen text-cwhite font-gotham-bold">
-            <div className="grid grid-cols-2 gap-4 h-screen items-center justify-center">
+            {/* <div className="grid grid-cols-2 gap-4 h-screen items-center justify-center">
                 <div className='col-start-1 col-end-1'>
                     <div className="hero min-h-full">
                         <div className="hero-content flex-col lg:flex-row">
@@ -66,6 +66,33 @@ const Home = () => {
                                 <img src={portfolio} className="" alt="logo" />
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div> */}
+            <div className='h-screen flex lg:flex-row md:flex-row items-center lg:justify-evenly sm justify-center sm:flex-col flex-col'>
+                <div className='flex flex-col'>
+                    <h1 className="lg:text-5xl md:text-3xl">Hello ! I'm <span className=" font-gotham-ultra-italic"> <span className=' md:ml-2 sm:ml-2 ml-4'>
+                        <TypeAnimation
+                            sequence={[
+                                'Lukas Portier',
+                                5000,
+                                'Lytzeer',
+                                5000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            className='text-gradient'
+                            style={{display: 'inline-block'}}
+                            repeat={Infinity}
+                        />
+                        <span className='text-cwhite'>,</span></span></span></h1>
+                    <p className="lg:text-4xl md:text-2xl"> 
+                        a French web and mobile developer.
+                    </p>
+                </div>
+                <div className="lg:flex-row min-w-minr">
+                    <div className=''>
+                        <img src={portfolio} className="" alt="logo" />
                     </div>
                 </div>
             </div>
