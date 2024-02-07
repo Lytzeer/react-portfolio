0301af3054e7c118b2030252afc3eea0be55f7ec
Lytzeer <lukasportier@gmail.com>
2024-02-07 10:07:33 +0100

    get all projects with json file

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -26,49 +26,13 @@ import Vscode from '../../assets/img/vs.png';
 import Wordpress from '../../assets/img/wordpress.png';
 import Linkedin from '../../assets/img/linkedin.png';
 import { TypeAnimation } from 'react-type-animation';
+import Projects from './data/projects.json';
 import React from 'react';
 
 const Home = () => {
+    var images = [pokemoncli, WeatherApp, monitoring, hangmanw, hangman, portfolio, Forum, ecommerce]    
     return (
         <div className="bg-cblack min-h-screen text-cwhite font-gotham-bold">
-            {/* <div className="grid grid-cols-2 gap-4 h-screen items-center justify-center">
-                <div className='col-start-1 col-end-1'>
-                    <div className="hero min-h-full">
-                        <div className="hero-content flex-col lg:flex-row">
-                            <div className=''>
-                                <h1 className="lg:text-5xl md:text-3xlflex">Hello ! I'm <span className=" font-gotham-ultra-italic"> <span className='ml-4'>
-                                    <TypeAnimation
-                                        sequence={[
-                                            // Same substring at the start will only be typed out once, initially
-                                            'Lukas Portier',
-                                            5000, // wait 1s before replacing "Mice" with "Hamsters"
-                                            'Lytzeer',
-                                            5000,
-                                        ]}
-                                        wrapper="span"
-                                        speed={50}
-                                        className='text-gradient'
-                                        style={{display: 'inline-block'}}
-                                        repeat={Infinity}
-                                    />
-                                    <span className='text-cwhite'>,</span></span></span></h1>
-                                <p className="lg:text-4xl md:text-2xl"> 
-                                    a French web and mobile developer.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-start-2 col-end-2 max-h-fit'>
-                    <div className="hero min-h-screen justify-center">
-                        <div className="hero-content flex-col lg:flex-row min-w-minr">
-                            <div className=''>
-                                <img src={portfolio} className="" alt="logo" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <div className='h-screen flex lg:flex-row md:flex-row items-center lg:justify-evenly sm justify-center sm:flex-col flex-col'>
                 <div className='flex flex-col'>
                     <h1 className="lg:text-5xl md:text-3xl sm:text-3xl text-3xl">Hello ! I'm <span className=" font-gotham-ultra-italic"> <span className=' md:ml-2 sm:ml-2 ml-4'>
@@ -103,19 +67,25 @@ const Home = () => {
                     </span>
                 </h1>
                 <div className='flex flex-wrap justify-center w-full'>
-                    <div className="card w-96 h-96 bg-base-100 shadow-xl image-full mx-8 mb-8 card-shadow">
-                        <figure><img src={pokemoncli} alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title text-cwhite">Pokemon CLI</h2>
-                            <p className='text-sm text-cwhite'>Pokemon Combat System in Python</p>
-                            <div className="card-actions justify-end">
-                                <a href="https://github.com/Lytzeer/PokemonPython" target='blank'>
-                                    <button className="btn btn-primary text-cwhite tracking-wider button-gradient hover:button-gradient-hover">SEE MORE</button>
-                                </a>
+                    {Projects.project.map((project, index) => {
+                        return(
+                            <div className="card w-96 h-96 bg-base-100 shadow-xl image-full mx-8 mb-8 card-shadow">
+                                <figure><img src={images[index]} alt={project.name} /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title text-cwhite">{project.name}</h2>
+                                    <p className='text-sm text-cwhite'>{project.description}</p>
+                                    <div className="card-actions justify-end">
+                                        <a href={project.repo_url} target='blank'>
+                                            <button className="btn btn-primary text-cwhite tracking-wider button-gradient hover:button-gradient-hover">SEE MORE</button>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="card w-96 h-96 bg-base-100 shadow-xl image-full mx-8 mb-8 card-shadow">
+                        )
+                    }
+                    )}
+                    
+                    {/* <div className="card w-96 h-96 bg-base-100 shadow-xl image-full mx-8 mb-8 card-shadow">
                         <figure><img src={hangman} alt="Shoes" /></figure>
                         <div className="card-body">
                             <h2 className="card-title text-cwhite">Hangman</h2>
@@ -198,7 +168,7 @@ const Home = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='flex flex-col justify-center text-center'>
                     <h1 className='text-gradient pt-28 pb-8'>
