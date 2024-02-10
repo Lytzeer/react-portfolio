bc7bc81d01294b4058195877956a3ec962e4e18f
Lytzeer <lukasportier@gmail.com>
2024-02-10 22:25:31 +0100

    split components for Project section

---
+++ src/Components/Home/Projects_Components/Title.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <h1 className='text-gradient pb-8'>
+            <span className='mx-4'>
+                MY PROJECTS
+            </span>
+        </h1>
+    )
+};
+
+export default Title;
