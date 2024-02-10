aeb1a51d8a735a218a5e7584655cdf5243b6ae6f
Lytzeer <lukasportier@gmail.com>
2024-02-10 22:32:49 +0100

    split components for Tech Stack section

---
+++ src/Components/Home/Stack_Components/Title.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <h1 className='text-gradient pt-28 pb-8'>
+            <span className='mx-4'>
+                MY TECH STACK
+            </span>
+        </h1>
+    )
+};
+
+export default Title;
